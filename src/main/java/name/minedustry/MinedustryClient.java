71066ef3f52f83f7b65e6d2fb7e6cbf94258cbee
Ken <keniis@126.com>
2024-07-13 16:19:24 +0800

    Initial commit

---
+++ src/main/java/name/minedustry/MinedustryClient.java
@@ -0,0 +1,11 @@
+package name.minedustry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinedustryClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
